86167e4134c994f62fb53d30da3c543fe77bc8a7
Uladzislau Zubtsou <uladzislau_zubtsou@epam.com>
2019-06-21 11:16:59 +0300

    Task 6 final

--- task6.py
+++ task6.py
@@ -1,19 +1,15 @@
-#mask_number = int(input("How many mask? "))
-mask_number = 2
+mask_number = int(input("How many mask? "))
 masks = []
-#for i in range(mask_number):
-#    masks += [input().split(".")]
-masks = [['255', '255', '255', '0'],['255', '255', '0', '0']]
-#ip_pairs = int(input("How many addresses pairs? "))
-ip_pairs = 1
 
+for i in range(mask_number):
+    masks += [input().split(".")]
+ip_pairs = int(input("How many addresses pairs? "))
 
 for i in range(ip_pairs):
-    #ip_addr1, ip_addr2 = input().split()
-    #ip_raw1 = ip_addr1.split(".")
-    #ip_raw2 = ip_addr2.split(".")
-    ip_raw1 = ['192', '168', '8', '8']
-    ip_raw2 = ['192', '168', '9', '2']
+    commons = 0
+    ip_addr1, ip_addr2 = input().split()
+    ip_raw1 = ip_addr1.split(".")
+    ip_raw2 = ip_addr2.split(".")
     for j in range(mask_number):
         full_leng1 = format(int(ip_raw1[0]), '08b') + format(int(ip_raw1[1]), '08b') + format(int(ip_raw1[2]), '08b') + format(int(ip_raw1[3]), '08b')
         full_leng2 = format(int(ip_raw2[0]), '08b') + format(int(ip_raw2[1]), '08b') + format(int(ip_raw2[2]), '08b') + format(int(ip_raw2[3]), '08b')
@@ -22,7 +18,6 @@ for i in range(ip_pairs):
         full_leng1 = int(full_leng1[0:lengs])
         full_leng2 = int(full_leng2[0:lengs])
         full_mask = int(full_mask[0:lengs])
-        if (full_leng1 == full_leng2):
-            print("ok")
-        else:
-            print("not ok")
+        if full_leng1 == full_leng2:
+            commons += 1
+    print(commons)

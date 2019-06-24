c25f15ef532756f2ed2a9dbefecb1aff9741e829
Uladzislau Zubtsou <uladzislau_zubtsou@epam.com>
2019-06-24 18:22:32 +0300

    Draft of program

--- pr-stats.py
+++ pr-stats.py
@@ -1,19 +1,27 @@
 # TODO script to get PR(Pull Request) statistics from GitHub.
-# TODO Show help.
-# TODO Print the program's installed version
-# TODO Basic statistics about merged/closed rate.
-# TODO Number of days opened.
-# TODO Number of comments created.
-# TODO Day of the week opened.
+# Usage:
+# pr-stats [options] <user> [<repo>]
+# pr-stats --version
+# pr-stats (-h | --help)
+# TODO -h | --help Show help.
+# TODO -v | --version Print the program's installed version
+# TODO -s | --base-stat Basic statistics about merged/closed rate.
+# TODO --day-opened Number of days opened.
+# TODO --comments Number of comments created.
+# TODO --day ? Day of the week opened.
 # TODO Hour of the day opened.
-# TODO Week opened.
-# TODO User who opened.
-# TODO User who closed.
-# TODO Attached labels.
+# TODO --week-opened Week opened.
+# TODO -u | --user User who opened.
+# TODO --closed-by User who closed.
+# TODO --labels Attached labels.
 # TODO Number of lines added.
 # TODO Number of lines deleted.
 # TODO Option to consider only pull requests opened on or after this date.
 # TODO Only consider pull requests opened before this date.
+# argparse https://docs.python.org/3.7/howto/argparse.html
+# requests http://docs.python-requests.org/en/master/user/quickstart/
+# getpass https://docs.python.org/3.7/library/getpass.html
+# configparser https://docs.python.org/3/library/configparser.html
 
 import argparse
 import requests
@@ -21,9 +29,17 @@ import getpass
 import configparser
 import json
 
-# ("fb99359949c1556bcad2606e1388a29e857f029c")
-r = requests.get('https://api.github.com/repos/alenaPy/devops_lab/pulls', headers={'Authorization': 'token fb99359949c1556bcad2606e1388a29e857f029c'})
-json_string = r.json()
-for dictinory in json_string:
-    print(dictinory['title'])
+# TODO add class for storing all dump and methods for processing this dump
 
+line = argparse.ArgumentParser()
+url = "https://api.github.com/repos/alenaPy/devops_lab/pulls"
+token = "06d3a96a3c90b8acd0012555546a84d3b61db745"
+token = "token {}".format(token)
+
+gh_dump = requests.get(url, headers={"Authorization": token})
+gh_local = gh_dump.json()
+
+while 'next' in gh_dump.links.keys():
+  gh_dump=requests.get(gh_dump.links['next']['url'], headers={"Authorization": token}) # how its work
+  gh_local.extend(gh_dump.json())
+print(len(gh_local))
